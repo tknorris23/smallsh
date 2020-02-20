7a1f85dba4ea9cf128b38344de2c1c8d6502fc27
tknorris23 <tknorris23@gmail.com>
2020-02-19 19:43:19 -0800

    prompt now recognizes built-in commands from input

--- smallsh.c
+++ smallsh.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
+void shexit()
+{
+    printf("exit called!\n");
+}
+
+void shcd()
+{
+    printf("cd called!\n");
+}
+
+void shstatus()
+{
+    printf("status called!\n");
+}
 
 main()
 {
@@ -8,14 +22,29 @@ main()
     //while loop for the prompt
     while(end == 0)
     {
-        if(end == 0)
+        //THE PROMPT
+        //prints colon then gets input from stdin
+        printf(":");
+        char input[2048];
+        memset(input, 0, sizeof(input));
+        fgets(input, 2048, stdin);
+        //BUILT-IN COMMANDS
+        //check for exit
+        //I don't know why when input is "exit", strcmp returns 10
+        //probably a problem to be solved later!
+        if (strcmp(input, "exit") == 10)
         {
-            //THE PROMPT
-            //prints colon then gets input from stdin
-            printf(":");
-            char input[2048];
-            memset(input, 0, sizeof(input));
-            fgets(input, 500, stdin);
+            shexit();
+        }
+        //check for cd
+        if (strcmp(input, "cd") == 10)
+        {
+            shcd();
+        }
+        //check for status
+        if (strcmp(input, "status") == 10)
+        {
+            shstatus();
         }
     }
 }
