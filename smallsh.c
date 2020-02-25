2f1b11980193f88dfe02d7c566a5c9adbf7a806a
tknorris23 <tknorris23@gmail.com>
2020-02-24 19:11:16 -0800

    Added exec() redirects and functionality for status built in command. cd is broken as hell.

--- smallsh.c
+++ smallsh.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <fcntl.h>
 
 //"exit" built-in command function
 void shexit()
@@ -15,24 +16,16 @@ void shexit()
 }
 
 
-
-
-
-
 //"cd" built-in command function
 void shcd(char** cwd, char* input, const char* PATH);
 
 //"status" built-in command function
-void shstatus()
-{
-    printf("status called!\n");
-}
+void shstatus(int exit, int sig, int exitORsig);
 
 main()
 {
     //set working directory
     const char *PATH = getenv("HOME");
-    printf("PATH gotten is: %s\n", PATH);
     char* cwd;
     cwd = (char *)malloc(50);
     char* buf;
@@ -40,11 +33,13 @@ main()
     size_t size;
     size = 1024;
     getcwd(cwd, size);
-    printf("cwd gotten is: %s\n", cwd);
+    //printf("cwd gotten is: %s\n", cwd);
 
 
+    //looping vars
     int end = 0;
     int i;
+    int j;
     char input[2048];
     char *command[518];
     char *command2[518];
@@ -54,9 +49,24 @@ main()
     argc2 = 0;
     char *p;
 
+    //pseudo bools for redirection info
+    int passin, passout, bac;
+    passin = 0;
+    passout = 0;
+    bac = 0;
+    //int for keeping track of redirection
+    int index_in = 0;
+    int index_out = 0;
+    int saved_stdin, saved_stdout;
+
+    //ints for checking exit status
+    int exitStatus = 0;
+    int termSignal = 0;
+    int exitORsig = 0; // 0 = exit, 1 = signal
+
     //exec() vars
     pid_t spawnPid = -5;
-    int childExitStatus = -5;
+    int childExitMethod = -5;
     //while loop for shell prompt
     while(end == 0)
     {
@@ -88,22 +98,76 @@ main()
         strcpy(p, input);
         char *tok = NULL;
         i = 0;
+        j = 0;
         for (tok = strtok (p, " "); tok; tok = strtok(NULL, " \n"))
         {
+            //set command[i] to value of token
             char *output_tok = strdup(tok);
             command[i] = (char *)malloc(sizeof(output_tok));
             strcpy(command[i], output_tok);
             argc1++;
+            //set value of command2[i] to value of token, given it's not a redirection character
             if (strcmp(output_tok, "<") != 0 && strcmp(output_tok, ">") != 0 && strcmp(output_tok, "&") != 0)
             {
-                command2[i] = (char *)malloc(sizeof(output_tok));
-                strcpy(command2[i], output_tok);
+                command2[j] = (char *)malloc(sizeof(output_tok));
+                strcpy(command2[j], output_tok);
                 argc2++;
             }
+            //if token is a redirect chacter, decrement j so that there's no empty array indexes
+            else
+            {
+                j--;
+            }
+            //check for redirects
+            //in redir
+            if(strcmp(output_tok, "<") == 0)
+            {
+                printf("caught a <.\n");
+                passin = 1;
+                index_in = i;
+                //file to get data from will be index_in + 1.
+            }
+            //out redir
+            if(strcmp(output_tok, ">") == 0)
+            {
+                printf("caught a >.\n");
+                passout = 1;
+                index_out = i;
+            }
+            //check for ampersand command
+            if(strcmp(output_tok, "&") ==0)
+            {
+                bac = 1;
+            }
+            //check for $$ to expand into pid
+            if(strcmp(output_tok, "$$") == 0)
+            {
+                //get PID of shell
+                pid_t shell;
+                shell = getpid();
+                //set up vars to change int to string
+                int len = snprintf(NULL, 0, "%d", shell);
+                char* str = malloc(len + 1);
+                //get string of pid value
+                snprintf(str, len+1, "%d", shell);
+                //Switch command[i] and command2[j] with the PID
+                memset(command[i], '\0', sizeof(command[i]));
+                memset(command2[j], '\0', sizeof(command2[j]));
+                command[i] = (char *)malloc(sizeof(str));
+                command2[j] = (char *)malloc(sizeof(str));
+                memset(command[i], '\0', sizeof(str));
+                strcpy(command[i], str);
+                //printf("$$ detected, now: [%d]\n", command[i]);
+            }
+            //free token var
             free(output_tok);
+            //increment looping vars
             i++;
+            j++;
         }
+        //reset vars
         i = 0;
+        j = 0;
         command[argc1] = NULL;
         command2[argc2] = NULL;
 
@@ -116,17 +180,31 @@ main()
         //check for cd
         else if (strcmp(command[0], "cd") == 0)
         {
+            //check if it's a HOME cd or one with a path
             char* pth;
-            pth = (char *) malloc(sizeof(command[1]));
-            strcpy(pth, command[1]);
+            //if an input is used, set pth to be sent to shcd
+            if (command[1] != NULL)
+            {
+                printf("about to set pth\n");
+                pth = (char *) malloc(sizeof(command[1]));
+                strcpy(pth, command[1]);
+                printf("copied input: [%s]\n", pth);
+            }
+            //if it's a HOME call, set pth to "-1"
+            else
+            {
+                pth = (char *)malloc(3);
+                strcpy(pth, "-1");
+            }
+            //call shcd
             shcd(&cwd, pth, PATH);
+            //reset pth
             strcpy(pth, "");
         }
         //check for status
         else if (strcmp(command[0], "status") == 0)
         {
-            
-            shstatus();
+            shstatus(exitStatus, termSignal, exitORsig);
         }
         //check for nothing
         else if (strcmp(command[0], "") == 0)
@@ -141,30 +219,88 @@ main()
         //exec() commands
         else
         {
-            printf("command2[0] is: %s\n", command2[0]);
+            //printf("command2[0] is: %s\n", command2[0]);
+            //check for background execution
+            if(bac)
+            {
+                //this is gonna be handled differently when I work on background commands
+            }
+            //check for redirects
+            if(passin)
+            {
+                //make vars for function use
+                int fdin;
+                int result;
+                //save stdin's file descriptor for later
+                saved_stdin = dup(0);
+                //open file from given path for read only
+                fdin = open(command[index_in+1], O_RDONLY);
+                //set stdin to be from file
+                result = dup2(fdin, 0);
+                close(fdin);
+                result = 0;
+            }
+            if(passout)
+            {
+                int fdout;
+                int result;
+                //save stdout's file descriptor for later
+                saved_stdout = dup(1);
+                //open file from given path for write only (will wipe file if it has data)
+                fdout = open(command[index_out+1], O_WRONLY);
+                //set stdout to send to file
+                result = dup2(fdout, 1);
+                //printf("result of dup2 = [%d]\n", result);
+                close(fdout);
+                result = 0;
+            }
+            //fork process
             spawnPid = fork();
             switch (spawnPid)
             {
-                case -1: { perror("Fork failed!\n"); exit(1); break;}
+                case -1: { perror("Fork failed!\n"); exit(1); break;} // error case
                 case 0: {
                     //child
-                    printf("[%s]\n", cwd);
-                    printf("Child process running...\n");
+                    //printf("[%s]\n", cwd);
+                    //printf("Child process running...\n");
+                    //execute command
                     execvp(command2[0], command2);
-                    printf("exec failed: %s\n", strerror(errno));
+                    //printf("exec failed: %s\n", strerror(errno));
                     perror("CHILD: exec failed.\n");
                     exit(2); break;
                 }
                 default: {
                     //parent
-                    printf("child(%d) spawned, waiting...\n", spawnPid);
-                    pid_t actualPid = waitpid(spawnPid, &childExitStatus, 0);
-                    printf("Done, child(%d) terminated.\n", actualPid);
+                    //printf("child(%d) spawned, waiting...\n", spawnPid);
+                    //wait for child to exit
+                    pid_t actualPid = waitpid(spawnPid, &childExitMethod, 0);
+                    //check exit method for child, and set vars as appropriate
+                    if (WIFEXITED(childExitMethod) != 0)
+                    {
+                        exitStatus = WEXITSTATUS(childExitMethod);
+                        exitORsig = 0;
+                    }
+                    else if (WIFSIGNALED(childExitMethod) != 0)
+                    {
+                        termSignal = WTERMSIG(childExitMethod);
+                        exitORsig = 1;
+                    }
+                    //printf("Done, child(%d) terminated.\n", actualPid);
                 }
             }
         }
 
-        printf("resetting command arrays\n");
+        //reset file descriptors
+        if(passin)
+        {
+            dup2(saved_stdin, 0);
+            close(saved_stdin);
+        }
+        if(passout)
+        {
+            dup2(saved_stdout, 1);
+            close(saved_stdout);
+        }
         //reset command array
         for(i = 0; i < 518; i++)
         {
@@ -173,7 +309,10 @@ main()
             free(command2[i]);
             argc2 = 0;
         }
-        printf("done with commands\n");
+        //reset other prompt vars
+        passin = 0;
+        passout = 0;
+        bac = 0;
     }
 }
 
@@ -181,52 +320,60 @@ main()
 //"cd" built-in command function
 void shcd(char** cwd, char* input, const char* PATH)
 {
+    //this function sucks ass and leads to so many memory issues because it's all string bs
+    //it's gonna be rewritten before wednesday :)
     printf("cd called!\n");
-    printf("cwd is: '%s'\n", *cwd);
-    printf("input is: '%s'\n", input);
-    printf("PATH is: '%s'\n", PATH);
+    printf("cwd is: [%s]\n", *cwd);
+    printf("input is: [%s]\n", input);
+    printf("PATH is: [%s]\n", PATH);
 
-    //create relative path
-    char pathname[1024];
-    memset(pathname, '\0', sizeof(pathname));
-    strcpy(pathname, *cwd);
-    strcat(pathname, input);
-    printf("relative pathname: '%s'\n", pathname);
+    if (strcmp(input, "-1") != 0)
+    {
+        //create relative path
+        char *pathname;
+        pathname = (char *)malloc(sizeof(*cwd) + sizeof(input) + 1);
+        memset(pathname, '\0', sizeof(pathname));
+        strcpy(pathname, *cwd);
+        strcat(pathname, "/");
+        strcat(pathname, input);
+        printf("relative pathname: '%s'\n", pathname);
 
-    //test for absolute path
-    DIR *abs;
-    abs = opendir(input);
-    if (abs)
-    {
-        printf("absolute path found.\n");
-        *cwd = (char *)malloc(sizeof(input));
-        memset(*cwd, '\0', sizeof(input));
-        strcpy(*cwd, input);
-        chdir(*cwd);
-        printf("cwd changed to: '%s'\n", *cwd);
-    }
-    else if (ENOENT == errno)
-    {
-        printf("absolute path not found.\n");
-    }
-    closedir(abs);
-    DIR *rel;
-    rel = opendir(pathname);
-    if (rel && strcmp(input, "") != 0)
-    {
-        printf("relative path found.\n");
-        *cwd = (char *)malloc(sizeof(pathname));
-        memset(*cwd, '\0', sizeof(pathname));
-        strcpy(*cwd, pathname);
-        chdir(*cwd);
-        printf("cwd changed to: '%s'\n", *cwd);
-    }
-    else if (ENOENT == errno)
-    {
-        printf("relative path not found.\n");
-    }
+        //test for absolute path
+        DIR *abs;
+        abs = opendir(input);
+        if (abs)
+        {
+            printf("absolute path found.\n");
+            *cwd = (char *)malloc(sizeof(input));
+            memset(*cwd, '\0', sizeof(input));
+            strcpy(*cwd, input);
+            chdir(*cwd);
+            printf("cwd changed to: '%s'\n", *cwd);
+        }
+        else if (ENOENT == errno)
+        {
+            printf("absolute path not found.\n");
+        }
+        closedir(abs);
+        DIR *rel;
+        rel = opendir(pathname);
+        if (rel)
+        {
+            printf("relative path found.\n");
+            *cwd = (char *)malloc(sizeof(pathname));
+            memset(*cwd, '\0', sizeof(pathname));
+            strcpy(*cwd, pathname);
+            chdir(*cwd);
+            printf("cwd changed to: '%s'\n", *cwd);
+        }
+        else if (ENOENT == errno)
+        {
+            printf("relative path not found.\n");
+        }
 
-    if (strcmp(input, "") == 0)
+    }
+    
+    else if (strcmp(input, "-1") == 0)
     {
         printf("null call\n");
         *cwd = (char *)malloc(sizeof(PATH));
@@ -235,6 +382,18 @@ void shcd(char** cwd, char* input, const char* PATH)
         chdir(*cwd);
         printf("cwd changed to: '%s'\n", *cwd);
     }
-    
+}
 
+void shstatus(int exit, int sig, int exitORsig) 
+{
+    //exitORsig = 0 if child exited normally
+    if(exitORsig)
+    {
+        printf("terminating signal: [%d]\n", sig);
+    }
+    //exitORsig = 1 if child exited from a signal
+    else if (!exitORsig)
+    {
+        printf("exit status: [%d]\n", exit);
+    }
 }
